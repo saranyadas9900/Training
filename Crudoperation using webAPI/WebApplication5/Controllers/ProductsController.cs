c84f53062499c713f086f0f47de2972022596f6d
saranyadas9900 <163109842+saranyadas9900@users.noreply.github.com>
2024-08-19 21:38:03 +0530

    Add files via upload

---
+++ Crudoperation using webAPI/WebApplication5/Controllers/ProductsController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication5.DAL;
+
+namespace WebApplication5.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductRepository _productRepository;
+
+        public ProductsController(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        // GET: api/products
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return Ok(_productRepository.GetAllProducts());
+        }
+
+        // GET: api/products/5
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var product = _productRepository.GetProductById(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        // POST: api/products
+        [HttpPost]
+        public ActionResult<Product> PostProduct(Product product)
+        {
+            _productRepository.AddProduct(product);
+
+            return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+        }
+
+        // PUT: api/products/5
+        [HttpPut("{id}")]
+        public IActionResult PutProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _productRepository.UpdateProduct(product);
+
+            return NoContent();
+        }
+
+        // DELETE: api/products/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = _productRepository.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _productRepository.DeleteProduct(id);
+
+            return NoContent();
+        }
+    }
+
+}
---
+++ Crudoperation using webAPI/WebApplication5/DAL/ProductRepository.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace WebApplication5.DAL
+{
+    public class ProductRepository
+    {
+        private readonly string _connectionString;
+
+        public ProductRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            var products = new List<Product>();
+
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_GetAllProducts", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    var product = new Product
+                    {
+                        Id = Convert.ToInt32(rdr["Id"]),
+                        Name = rdr["Name"].ToString(),
+                        Price = Convert.ToDecimal(rdr["Price"]),
+                        Stock = Convert.ToInt32(rdr["Stock"])
+                    };
+
+                    products.Add(product);
+                }
+
+                con.Close();
+            }
+
+            return products;
+        }
+
+        public Product GetProductById(int id)
+        {
+            Product product = null;
+
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_GetProductById", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", id);
+                con.Open();
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                if (rdr.Read())
+                {
+                    product = new Product
+                    {
+                        Id = Convert.ToInt32(rdr["Id"]),
+                        Name = rdr["Name"].ToString(),
+                        Price = Convert.ToDecimal(rdr["Price"]),
+                        Stock = Convert.ToInt32(rdr["Stock"])
+                    };
+                }
+
+                con.Close();
+            }
+
+            return product;
+        }
+
+        public void AddProduct(Product product)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_AddProduct", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Name", product.Name);
+                cmd.Parameters.AddWithValue("@Price", product.Price);
+                cmd.Parameters.AddWithValue("@Stock", product.Stock);
+                con.Open();
+
+                product.Id = Convert.ToInt32(cmd.ExecuteScalar());
+
+                con.Close();
+            }
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_UpdateProduct", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", product.Id);
+                cmd.Parameters.AddWithValue("@Name", product.Name);
+                cmd.Parameters.AddWithValue("@Price", product.Price);
+                cmd.Parameters.AddWithValue("@Stock", product.Stock);
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+            }
+        }
+
+        public void DeleteProduct(int id)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_DeleteProduct", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", id);
+                con.Open();
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+            }
+        }
+    }
+
+}
